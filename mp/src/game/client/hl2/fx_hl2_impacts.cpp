9223857f69d54c52a25817274aca30bc5ad18017
azzy <azzyr@protonmail.com>
2024-02-19 17:50:22 +0200

    add missing hl2 tempents

--- mp/src/game/client/hl2/fx_hl2_impacts.cpp
+++ mp/src/game/client/hl2/fx_hl2_impacts.cpp
@@ -80,39 +80,6 @@ void ImpactGaussCallback( const CEffectData &data )
 
 DECLARE_CLIENT_EFFECT( "ImpactGauss", ImpactGaussCallback );
 
-//-----------------------------------------------------------------------------
-// Purpose: Handle weapon impacts
-//-----------------------------------------------------------------------------
-void ImpactCallback( const CEffectData &data )
-{
-	VPROF_BUDGET( "ImpactCallback", VPROF_BUDGETGROUP_PARTICLE_RENDERING );
-
-	trace_t tr;
-	Vector vecOrigin, vecStart, vecShotDir;
-	int iMaterial, iDamageType, iHitbox;
-	short nSurfaceProp;
-	C_BaseEntity *pEntity = ParseImpactData( data, &vecOrigin, &vecStart, &vecShotDir, nSurfaceProp, iMaterial, iDamageType, iHitbox );
-
-	if ( !pEntity )
-	{
-		// This happens for impacts that occur on an object that's then destroyed.
-		// Clear out the fraction so it uses the server's data
-		tr.fraction = 1.0;
-		PlayImpactSound( pEntity, tr, vecOrigin, nSurfaceProp );
-		return;
-	}
-
-	// If we hit, perform our custom effects and play the sound
-	if ( Impact( vecOrigin, vecStart, iMaterial, iDamageType, iHitbox, pEntity, tr ) )
-	{
-		// Check for custom effects based on the Decal index
-		PerformCustomEffects( vecOrigin, tr, vecShotDir, iMaterial, 1.0 );
-	}
-
-	PlayImpactSound( pEntity, tr, vecOrigin, nSurfaceProp );
-}
-
-DECLARE_CLIENT_EFFECT( "Impact", ImpactCallback );
 
 //-----------------------------------------------------------------------------
 // Purpose: 
