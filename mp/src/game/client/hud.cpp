d69c320d12876dd1531f821db65820893ccd574c
azzy <azzyr@protonmail.com>
2024-03-02 05:21:54 +0200

    temporarily fix super obscure FFPanel crashes with evil hack

--- mp/src/game/client/hud.cpp
+++ mp/src/game/client/hud.cpp
@@ -603,6 +603,10 @@ void CHudTexture::Precache(void)
 
 void CHudTexture::DrawSelf(int x, int y, const Color& clr) const
 {
+	// FF TODO: THIS IS AWFUL / EVIL
+	if (this == reinterpret_cast<CHudTexture*>(0xFFEEFFEE))
+		return;
+
 	DrawSelf(x, y, Width(), Height(), clr);
 }
 
