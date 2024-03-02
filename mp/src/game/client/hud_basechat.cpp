69f1f0efe4cfe6c6d8a6b3ebefb9d6dace1b0da9
-_YoYo178_- <95132723+YoYo-178@users.noreply.github.com>
2024-03-02 06:04:44 +0530

    fix chat localization messing up

--- mp/src/game/client/hud_basechat.cpp
+++ mp/src/game/client/hud_basechat.cpp
@@ -197,14 +197,18 @@ wchar_t* ReadChatTextString( bf_read &msg, OUT_Z_BYTECAP(outSizeInBytes) wchar_t
 	const char* token = strtok(szString, " ");
 	while (token != NULL)
 	{
-		wchar_t *pBuf = g_pVGuiLocalize->Find(token);
-
-		if (!pBuf)
+		if (token[0] == '#')
+		{
+			wchar_t* pBuf = g_pVGuiLocalize->Find(token);
+			V_wcsncat(translated, pBuf, outSizeInBytes);
+		}
+		else
+		{
 			g_pVGuiLocalize->ConvertANSIToUnicode(token, temp, outSizeInBytes);
+			V_wcsncat(translated, temp, outSizeInBytes);
+		}
 
-		V_wcsncat(translated, (pBuf ? pBuf : temp), outSizeInBytes);
 		V_wcsncat(translated, L" ", outSizeInBytes);
-
 		token = strtok(NULL, " ");
 	}
 
