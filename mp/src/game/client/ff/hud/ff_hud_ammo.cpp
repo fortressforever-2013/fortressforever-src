601ee4e035ae1588195e8ce0d3b9a0215653a7c8
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2024-11-27 20:00:08 +0530

    Update HudAmmo, HudAmmoClip, HudAmmoInfo

--- mp/src/game/client/ff/hud/ff_hud_ammo.cpp
+++ mp/src/game/client/ff/hud/ff_hud_ammo.cpp
@@ -30,6 +30,15 @@
 
 using namespace vgui;
 
+#define AMMO_BACKGROUND_TEXTURE "hud/AmmoBG"
+#define AMMO_FOREGROUND_TEXTURE "hud/AmmoFG"
+#define AMMOCLIP_BACKGROUND_TEXTURE "hud/AmmoClipBG"
+#define AMMOCLIP_FOREGROUND_TEXTURE "hud/AmmoClipFG"
+#define AMMOINFO_BACKGROUND_TEXTURE "hud/AmmoInfoBG"
+#define AMMOINFO_FOREGROUND_TEXTURE "hud/AmmoInfoFG"
+
+extern Color GetCustomClientColor(int iPlayerIndex, int iTeamIndex/* = -1*/);
+
 //-----------------------------------------------------------------------------
 // Purpose: Displays current ammunition level
 //-----------------------------------------------------------------------------
@@ -39,11 +48,16 @@ class CHudAmmo : public CHudNumericDisplay, public CHudElement
 
 public:
 	
-	CHudAmmo(const char *pElementName);
+	CHudAmmo( const char *pElementName );
+	~CHudAmmo( void ); 
 	
 	virtual void Init();
 	virtual void Reset();
-	virtual void OnTick();
+	virtual void VidInit( void );
+	virtual void ApplySchemeSettings( IScheme* pScheme );
+	virtual void Paint( void );
+			void UpdateDisplay( void );
+			void CacheTextures( void );
 
 protected:
 	CHandle< C_BaseCombatWeapon > m_hCurrentActiveWeapon;
@@ -52,69 +66,75 @@ protected:
 	virtual int GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon);
 
 	int		m_iAmmo;
+
+private:
+	CHudTexture* m_pBGTexture;
+	CHudTexture* m_pFGTexture;
 };
 
 //-----------------------------------------------------------------------------
-// Purpose: Displays current ammunition level in clip
+// Purpose: Constructor
 //-----------------------------------------------------------------------------
-class CHudAmmoClip : public CHudAmmo
-{
-	DECLARE_CLASS_SIMPLE(CHudAmmoClip, CHudAmmo);
-
-public:
-
-	CHudAmmoClip(const char *pElementName) : BaseClass(pElementName)
-	{
-		// Hopefully not too late to do this
-		SetName("HudAmmoClip");
-	}
-
-	virtual int GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon)
-	{
-		if (!pWeapon)
-			return -1;
-
-		return pWeapon->Clip1();
-	}
-};
-
-//-----------------------------------------------------------------------------
-// Purpose: Displays current weapon & ammo
-//-----------------------------------------------------------------------------
-class CHudAmmoInfo : public CHudElement, public FFPanel
-{
-public:
-	CHudAmmoInfo(const char *pElementName) : CHudElement(pElementName), FFPanel(NULL, "HudAmmoInfo")
-	{
-		SetParent(g_pClientMode->GetViewport());
-		SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_SPECTATING | HIDEHUD_UNASSIGNED | HIDEHUD_WEAPONSELECTION);
-	}
-};
-
-//-----------------------------------------------------------------------------
-// Purpose: A copy/paste so that glyphs draw correctly
-//-----------------------------------------------------------------------------
-class CHudAmmoInfo2 : public CHudElement, public FFPanel
-{
-public:
-	CHudAmmoInfo2(const char *pElementName) : CHudElement(pElementName), FFPanel(NULL, "HudAmmoInfo2")
-	{
-		SetParent(g_pClientMode->GetViewport());
-		SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_SPECTATING | HIDEHUD_UNASSIGNED | HIDEHUD_WEAPONSELECTION);
-	}
-};
-
-DECLARE_HUDELEMENT(CHudAmmo);
-DECLARE_HUDELEMENT(CHudAmmoClip);
-DECLARE_HUDELEMENT(CHudAmmoInfo);
-DECLARE_HUDELEMENT(CHudAmmoInfo2);
-
 CHudAmmo::CHudAmmo(const char *pElementName) : BaseClass(NULL, "HudAmmo"), CHudElement(pElementName)
 {
 	SetParent(g_pClientMode->GetViewport());
 	SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_SPECTATING | HIDEHUD_UNASSIGNED | HIDEHUD_WEAPONSELECTION);
+
+	m_pBGTexture = NULL;
+	m_pFGTexture = NULL;
 }
 
+//-----------------------------------------------------------------------------
+// Purpose: Destructor
+//-----------------------------------------------------------------------------
+CHudAmmo::~CHudAmmo()
+{
+	if (m_pBGTexture)
+	{
+		delete m_pBGTexture;
+		m_pBGTexture = NULL;
+	}
+
+	if (m_pFGTexture)
+	{
+		delete m_pFGTexture;
+		m_pFGTexture = NULL;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CHudAmmo::ApplySchemeSettings(IScheme* pScheme)
+{
+	BaseClass::ApplySchemeSettings(pScheme);
+
+	// blame CHudNumericDisplay
+	Panel::SetFgColor( GetSchemeColor( "HUD_Tone_Default", Color( 199, 219, 255, 215 ), pScheme ) );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmo::Init()
+{
+	m_hCurrentActiveWeapon = NULL;
+	m_iAmmo = -1;
+	SetLabelText(L"");
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmo::VidInit()
+{
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
 int CHudAmmo::GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon)
 {
 	if (!pWeapon || !pPlayer)
@@ -122,32 +142,66 @@ int CHudAmmo::GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon)
 
 	return pPlayer->GetAmmoCount(pWeapon->GetPrimaryAmmoType());
 }
-void CHudAmmo::Init()
-{
-	ivgui()->AddTickSignal( GetVPanel(), 100 );
-	m_hCurrentActiveWeapon = NULL;
-	m_iAmmo = -1;
-	SetLabelText(L"");
-}
 
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
 void CHudAmmo::Reset()
 {
 	m_iAmmo = -1;
 }
 
-void CHudAmmo::OnTick()
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmo::SetAmmo(int iAmmo, bool bPlayAnimation)
 {
-	BaseClass::OnTick();
-	if (!m_pFFPlayer)
+	if (bPlayAnimation && m_iAmmo != iAmmo)
+	{
+		//const char *pszAnimation = (iAmmo == 0 ? "AmmoEmpty" : (iAmmo < 0 ? "AmmoDecreased" : "AmmoIncreased"));
+		
+		// Mulch: Disabling all hud animations except for health/armor
+		//g_pClientMode->GetViewportAnimationController()->StartAnimationSequence(pszAnimation);
+	}
+
+	m_iAmmo = iAmmo;
+	SetDisplayValue(iAmmo);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Load and precache the textures
+//-----------------------------------------------------------------------------
+void CHudAmmo::CacheTextures()
+{
+	if (!m_pBGTexture)
+	{
+		m_pBGTexture = new CHudTexture();
+		m_pBGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMO_BACKGROUND_TEXTURE);
+	}
+
+	if (!m_pFGTexture)
+	{
+		m_pFGTexture = new CHudTexture();
+		m_pFGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMO_FOREGROUND_TEXTURE);
+	}
+}
+
+void CHudAmmo::UpdateDisplay()
+{
+	C_FFPlayer* pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if (!pPlayer)
 		return;
 
-	C_BaseCombatWeapon *pWeapon = m_pFFPlayer->GetActiveWeapon();
+	C_BaseCombatWeapon *pWeapon = pPlayer->GetActiveWeapon();
 
 	int iAmmo = -1;
 
 	if (pWeapon && pWeapon->UsesPrimaryAmmo())
 	{
-		iAmmo = GetPlayerAmmo(m_pFFPlayer, pWeapon);
+		iAmmo = GetPlayerAmmo(pPlayer, pWeapon);
 	}
 
 	if ( iAmmo < 0 )
@@ -175,7 +229,156 @@ void CHudAmmo::OnTick()
 	}
 }
 
-void CHudAmmo::SetAmmo(int iAmmo, bool bPlayAnimation)
+
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmo::Paint()
+{
+	C_FFPlayer* pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if ( !pPlayer )
+		return;
+
+	UpdateDisplay();
+
+	Color cColor = GetCustomClientColor( -1, pPlayer->GetTeamNumber() );
+	cColor.setA(150);
+
+	// draw our background first
+	surface()->DrawSetTextureFile( m_pBGTexture->textureId, AMMO_BACKGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pBGTexture->textureId );
+	surface()->DrawSetColor( cColor );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+
+	surface()->DrawSetTextureFile( m_pFGTexture->textureId, AMMO_FOREGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pFGTexture->textureId );
+	surface()->DrawSetColor( GetFgColor() );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+
+	// then the numbers
+	BaseClass::Paint();
+}
+
+DECLARE_HUDELEMENT(CHudAmmo);
+
+//-----------------------------------------------------------------------------
+// Purpose: Displays current ammunition level in clip
+//-----------------------------------------------------------------------------
+class CHudAmmoClip : public CHudNumericDisplay, public CHudElement
+{
+	DECLARE_CLASS_SIMPLE(CHudAmmoClip, CHudNumericDisplay);
+
+public:
+	
+	CHudAmmoClip( const char *pElementName );
+	~CHudAmmoClip( void );
+	
+	virtual void Init();
+	virtual void Reset();
+	virtual void VidInit( void );
+	virtual void ApplySchemeSettings( IScheme* pScheme );
+	virtual void Paint( void );
+			void UpdateDisplay( void );
+			void CacheTextures( void );
+
+protected:
+	CHandle< C_BaseCombatWeapon > m_hCurrentActiveWeapon;
+
+	virtual void SetAmmo(int ammo, bool playAnimation);
+	virtual int GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon);
+
+	int		m_iAmmo;
+
+private:
+	CHudTexture* m_pBGTexture;
+	CHudTexture* m_pFGTexture;
+};
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CHudAmmoClip::CHudAmmoClip(const char *pElementName) : BaseClass(NULL, "HudAmmoClip"), CHudElement(pElementName)
+{
+	SetParent(g_pClientMode->GetViewport());
+	SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_SPECTATING | HIDEHUD_UNASSIGNED | HIDEHUD_WEAPONSELECTION);
+
+	m_pBGTexture = NULL;
+	m_pFGTexture = NULL;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Destructor
+//-----------------------------------------------------------------------------
+CHudAmmoClip::~CHudAmmoClip()
+{
+	if (m_pBGTexture)
+	{
+		delete m_pBGTexture;
+		m_pBGTexture = NULL;
+	}
+
+	if (m_pFGTexture)
+	{
+		delete m_pFGTexture;
+		m_pFGTexture = NULL;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::ApplySchemeSettings(IScheme* pScheme)
+{
+	BaseClass::ApplySchemeSettings(pScheme);
+
+	// blame CHudNumericDisplay
+	Panel::SetFgColor( GetSchemeColor( "HUD_Tone_Default", Color( 199, 219, 255, 215 ), pScheme ) );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::Init()
+{
+	m_hCurrentActiveWeapon = NULL;
+	m_iAmmo = -1;
+	SetLabelText(L"");
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::VidInit()
+{
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+int CHudAmmoClip::GetPlayerAmmo(C_FFPlayer *pPlayer, C_BaseCombatWeapon *pWeapon)
+{
+	if (!pWeapon )
+		return -1;
+
+	return pWeapon->Clip1();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::Reset()
+{
+	m_iAmmo = -1;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::SetAmmo(int iAmmo, bool bPlayAnimation)
 {
 	if (bPlayAnimation && m_iAmmo != iAmmo)
 	{
@@ -188,3 +391,210 @@ void CHudAmmo::SetAmmo(int iAmmo, bool bPlayAnimation)
 	m_iAmmo = iAmmo;
 	SetDisplayValue(iAmmo);
 }
+
+//-----------------------------------------------------------------------------
+// Purpose: Load and precache the textures
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::CacheTextures()
+{
+	if (!m_pBGTexture)
+	{
+		m_pBGTexture = new CHudTexture();
+		m_pBGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMOCLIP_BACKGROUND_TEXTURE);
+	}
+
+	if (!m_pFGTexture)
+	{
+		m_pFGTexture = new CHudTexture();
+		m_pFGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMOCLIP_FOREGROUND_TEXTURE);
+	}
+}
+
+void CHudAmmoClip::UpdateDisplay()
+{
+	C_FFPlayer* pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if (!pPlayer)
+		return;
+
+	C_BaseCombatWeapon *pWeapon = pPlayer->GetActiveWeapon();
+
+	int iAmmo = -1;
+
+	if (pWeapon && pWeapon->UsesPrimaryAmmo())
+	{
+		iAmmo = GetPlayerAmmo(pPlayer, pWeapon);
+	}
+
+	if ( iAmmo < 0 )
+	{
+		SetPaintEnabled(false);
+		SetPaintBackgroundEnabled(false);
+		return;
+	}
+	else
+	{
+		SetPaintEnabled(true);
+		SetPaintBackgroundEnabled(true);
+	}
+
+	if (pWeapon == m_hCurrentActiveWeapon)
+	{
+		// Same weapon, update w/ animations
+		SetAmmo(iAmmo, true);
+	}
+	else
+	{
+		// Different weapon, update w/o animations
+		SetAmmo(iAmmo, false);
+		m_hCurrentActiveWeapon = pWeapon;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoClip::Paint()
+{
+	C_FFPlayer* pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if ( !pPlayer )
+		return;
+
+	UpdateDisplay();
+
+	Color cColor = GetCustomClientColor( -1, pPlayer->GetTeamNumber() );
+	cColor.setA(150);
+
+	// draw our background first
+	surface()->DrawSetTextureFile( m_pBGTexture->textureId, AMMOCLIP_BACKGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pBGTexture->textureId );
+	surface()->DrawSetColor( cColor );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+
+	surface()->DrawSetTextureFile( m_pFGTexture->textureId, AMMOCLIP_FOREGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pFGTexture->textureId );
+	surface()->DrawSetColor( GetFgColor() );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+
+	// then the numbers
+	BaseClass::Paint();
+}
+
+DECLARE_HUDELEMENT(CHudAmmoClip);
+
+//-----------------------------------------------------------------------------
+// Purpose: Displays current weapon & ammo
+//-----------------------------------------------------------------------------
+class CHudAmmoInfo : public CHudElement, public Panel
+{
+	DECLARE_CLASS_SIMPLE( CHudAmmoInfo, Panel );
+
+public:
+	CHudAmmoInfo( const char* pElementName );
+	~CHudAmmoInfo( void );
+	virtual void Init( void );
+	virtual void VidInit( void );
+	virtual void Paint( void );
+			void CacheTextures( void );
+
+private:
+	CHudTexture* m_pBGTexture;
+	CHudTexture* m_pFGTexture;
+};
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CHudAmmoInfo::CHudAmmoInfo( const char* pElementName ) : CHudElement( pElementName ), Panel( NULL, "HudAmmoInfo" )
+{
+	SetParent(g_pClientMode->GetViewport());
+	SetHiddenBits(HIDEHUD_PLAYERDEAD | HIDEHUD_SPECTATING | HIDEHUD_UNASSIGNED | HIDEHUD_WEAPONSELECTION);
+
+	m_pBGTexture = NULL;
+	m_pFGTexture = NULL;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Destructor
+//-----------------------------------------------------------------------------
+CHudAmmoInfo::~CHudAmmoInfo()
+{
+	if (m_pBGTexture)
+	{
+		delete m_pBGTexture;
+		m_pBGTexture = NULL;
+	}
+
+	if (m_pFGTexture)
+	{
+		delete m_pFGTexture;
+		m_pFGTexture = NULL;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoInfo::Init()
+{
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoInfo::VidInit()
+{
+	CacheTextures();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Load and precache textures
+//-----------------------------------------------------------------------------
+void CHudAmmoInfo::CacheTextures()
+{
+	if (!m_pBGTexture)
+	{
+		m_pBGTexture = new CHudTexture();
+		m_pBGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMOINFO_BACKGROUND_TEXTURE);
+	}
+
+	if (!m_pFGTexture)
+	{
+		m_pFGTexture = new CHudTexture();
+		m_pFGTexture->textureId = vgui::surface()->CreateNewTextureID();
+		PrecacheMaterial(AMMOINFO_FOREGROUND_TEXTURE);
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CHudAmmoInfo::Paint()
+{
+	C_FFPlayer* pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if ( !pPlayer )
+		return;
+
+	Color cColor = GetCustomClientColor( -1, pPlayer->GetTeamNumber() );
+	cColor.setA(150);
+
+	// draw our background first
+	surface()->DrawSetTextureFile( m_pBGTexture->textureId, AMMOINFO_BACKGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pBGTexture->textureId );
+	surface()->DrawSetColor( cColor );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+
+	surface()->DrawSetTextureFile( m_pFGTexture->textureId, AMMOINFO_FOREGROUND_TEXTURE, true, false );
+	surface()->DrawSetTexture( m_pFGTexture->textureId );
+	surface()->DrawSetColor( GetFgColor() );
+	surface()->DrawTexturedRect( 0, 0, GetWide(), GetTall() );
+}
+
+
+DECLARE_HUDELEMENT(CHudAmmoInfo);
