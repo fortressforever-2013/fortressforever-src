c75172a4c32f76352616bb88bd6b75f6b91bf326
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2025-02-06 16:53:05 -0600

    `string.rep` now accepts an optional separator argument
    https://github.com/dubiousjim/luafiveq/blob/6bc64192eff0a2c42b0c9bfd59fe3b72fd19b0ce/src/fiveq.c#L290

--- mp/src/thirdparty/lua-5.1.5/src/lstrlib.c
+++ mp/src/thirdparty/lua-5.1.5/src/lstrlib.c
@@ -91,14 +91,21 @@ static int str_upper (lua_State *L) {
 }
 
 static int str_rep (lua_State *L) {
-  size_t l;
-  luaL_Buffer b;
+  size_t l, lsep;
   const char *s = luaL_checklstring(L, 1, &l);
   int n = luaL_checkint(L, 2);
-  luaL_buffinit(L, &b);
-  while (n-- > 0)
-    luaL_addlstring(&b, s, l);
-  luaL_pushresult(&b);
+  const char *sep = luaL_optlstring(L, 3, "", &lsep);
+  if (n <= 0) lua_pushliteral(L, "");
+  else {
+    luaL_Buffer b;
+    luaL_buffinit(L, &b);
+    while(n-- > 1) { /* first n-1 copies (followed by separator) */
+      luaL_addlstring(&b, s, l);
+      luaL_addlstring(&b, sep, lsep);
+    }
+    luaL_addlstring(&b, s, l); /* last copy (not followed by separator) */
+    luaL_pushresult(&b);
+  }
   return 1;
 }
 
