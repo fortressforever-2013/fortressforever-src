91c20de3f6c34f80ede4cb3e242391f0839a374f
hlieb <220946691+ha1lieb@users.noreply.github.com>
2026-06-06 20:01:07 +0200

    Nailguns have real knockback as they used to have

--- mp/src/game/shared/ff/projectiles/ff_projectile_nail.cpp
+++ mp/src/game/shared/ff/projectiles/ff_projectile_nail.cpp
@@ -25,7 +25,7 @@
 
 //ConVar ffdev_nail_speed("ffdev_nail_speed", "1000.0", FCVAR_FF_FFDEV_REPLICATED , "Nail speed");
 //ConVar ffdev_nail_pushmultiplier("ffdev_nail_pushmultiplier", "0.05", FCVAR_FF_FFDEV_REPLICATED, "Nail pushforce multiplier - was 0.1 for 2.1 release");
-#define FF_NAIL_PUSHMULTIPLIER 0.05f //ffdev_nail_pushmultiplier.GetFloat()
+#define FF_NAIL_PUSHMULTIPLIER 0.7f //ffdev_nail_pushmultiplier.GetFloat()
 #define NAIL_SPEED 2000.0f //ffdev_nail_speed.GetFloat() //2000.0f and then 1000.0f and now 2000.0f again because 1000 is way too slow
 //ConVar ffdev_nail_bbox("ffdev_nail_bbox", "2.0", FCVAR_FF_FFDEV_REPLICATED, "Nail bbox");
 #define NAIL_BBOX 2.0f
