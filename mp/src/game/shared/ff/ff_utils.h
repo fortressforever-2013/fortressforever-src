de5751a7fb7c754d8475b512c55e192b5a6388b8
hlieb <220946691+ha1lieb@users.noreply.github.com>
2026-07-31 14:47:51 +0200

    Consistency tweak

--- mp/src/game/shared/ff/ff_utils.h
+++ mp/src/game/shared/ff/ff_utils.h
@@ -130,7 +130,7 @@ bool IsPlayerRadioTagTarget( CFFPlayer *pPlayer, int iTeamDoingTargetting );
 
 void FF_DecalTrace( CBaseEntity *pEntity, float flRadius, const char *pszDecalName );
 
-int FF_PrimingDLight(const char* pszGrenadeClassName);
+int FF_PrimingDLight(const char *pszGrenadeClassName);
 
 #ifdef GAME_DLL
 void FF_LuaHudIcon(CFFPlayer *pPlayer, const char *pszIdentifier, int x, int y, const char *pszImage, int iWidth, int iHeight, int iAlignX = -1, int iAlignY = -1);
